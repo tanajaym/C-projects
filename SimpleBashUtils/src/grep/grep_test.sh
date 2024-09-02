eb18ed03a7a39bcfdf0c2a8b11ac9db84389f219
Tana Jayme <tanajaym@pr-g1.kzn.21-school.ru>
2024-09-02 17:46:28 +0300

    SimpleBashUtils

---
+++ SimpleBashUtils/src/grep/grep_test.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+SUCCESS=0
+FAIL=0
+COUNTER=0
+DIFF_RES=""
+
+declare -a tests=(
+"s test_0_grep.txt VAR"
+"for s21_grep.c VAR"
+"-e for -e ^int s21_grep.c VAR"
+"-e regex -e ^print s21_grep.c VAR -f test_ptrn_grep.txt"
+"-e while -e void s21_grep.c Makefile VAR -f test_ptrn_grep.txt"
+)
+
+declare -a extra=(
+"-n for test_1_grep.txt test_2_grep.txt"
+"-n for test_1_grep.txt"
+"-n -e ^\} test_1_grep.txt"
+"-e ^int test_1_grep.txt"
+"-nivh = test_1_grep.txt test_2_grep.txt"
+"-e"
+"-ie INT test_5_grep.txt"
+"-echar test_1_grep.txt test_2_grep.txt"
+"-ne = -e out test_5_grep.txt"
+"-iv int test_5_grep.txt"
+"-in int test_5_grep.txt"
+"-v test_1_grep.txt -e ank"
+"-l for test_1_grep.txt test_2_grep.txt"
+"-e = -e out test_5_grep.txt"
+"-e ing -e as -e the -e not -e is test_6_grep.txt"
+"-l for no_file.txt test_2_grep.txt"
+)
+
+testing()
+{
+    t=$(echo $@ | sed "s/VAR/$var/")
+    ./s21_grep $t > test_s21_grep.log
+    grep $t > test_sys_grep.log
+    DIFF_RES="$(diff -s test_s21_grep.log test_sys_grep.log)"
+    (( COUNTER++ ))
+    if [ "$DIFF_RES" == "Files test_s21_grep.log and test_sys_grep.log are identical" ]
+    then
+      (( SUCCESS++ ))
+      echo "$FAIL \ $SUCCESS \ $COUNTER success grep $t"
+    else
+      (( FAIL++ ))
+      echo "$FAIL \ $SUCCESS \ $COUNTER fail grep $t"
+    fi
+    rm test_s21_grep.log test_sys_grep.log
+}
+
+# специфические тесты
+for i in "${extra[@]}"
+do
+    var="-"
+    testing $i
+done
+
+# 1 параметр
+for var1 in v c l n h 
+do
+    for i in "${tests[@]}"
+    do
+        var="-$var1"
+        testing $i
+    done
+done
+
+# 2 параметра без l
+for var1 in v c n h 
+do
+    for var2 in v c n h 
+    do
+        if [ $var1 != $var2 ]
+        then
+            for i in "${tests[@]}"
+            do
+                var="-$var1 -$var2"
+                testing $i
+            done
+        fi
+    done
+done
+
+# 2 параметра без c
+for var1 in v l n h 
+do
+    for var2 in v l n h 
+    do
+        if [ $var1 != $var2 ]
+        then
+            for i in "${tests[@]}"
+            do
+                var="-$var1 -$var2"
+                testing $i
+            done
+        fi
+    done
+done
+
+# 2 сдвоенных параметра без с
+for var1 in v l n h 
+do
+    for var2 in v l n h 
+    do
+        if [ $var1 != $var2 ]
+        then
+            for i in "${tests[@]}"
+            do
+                var="-$var1$var2"
+                testing $i
+            done
+        fi
+    done
+done
+
+# 2 сдвоенных параметра без l
+for var1 in v c n h 
+do
+    for var2 in v c n h 
+    do
+        if [ $var1 != $var2 ]
+        then
+            for i in "${tests[@]}"
+            do
+                var="-$var1$var2"
+                testing $i
+            done
+        fi
+    done
+done
+
+     
+echo "FAIL: $FAIL"
+echo "SUCCESS: $SUCCESS"
+echo "ALL: $COUNTER"
